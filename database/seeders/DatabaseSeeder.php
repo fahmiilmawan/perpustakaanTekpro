3336fd6e2673b08536b0bca2614922954f463179
unknown <fahmikeilmawan22@gmail.com>
2024-01-18 20:33:06 +0700

    Naon w Bebas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,13 @@ class DatabaseSeeder extends Seeder
     {
          \App\Models\User::factory(10)->create();
 
-//         \App\Models\User::factory()->create([
-//             'name' => 'Fahmi Ilmawan',
-//             'email' => 'user@gmail.com',
-//             'email_verified_at' => now(),
-//             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-//             'is_admin' => false,
-//             'remember_token' => Str::random(10),
-//         ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Fahmi Ilmawan',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'is_admin' => true,
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
